984177a05827debddebedc6fa09cc8ff976ed015
Michael Gabriel <mwgabriel@me.com>
2014-03-02 10:59:00 -0500

    added video

--- index.php
+++ index.php
@@ -42,6 +42,7 @@ exit();
         <p>Inverge offers a simple, yet effective, way for students to connect and brainstorm, collaborate and communicate, and share their visions. Inverge provides resources for students to innovate and succeed with their latest ventures!</p>
         <p><a class="btn btn-primary btn-lg" role="button" href="signup.php">Sign Up &raquo;</a></p>
       </div>
+      <embed width="420" height="345" src="http://www.youtube.com/v/XGSy3_Czz8k" type="application/x-shockwave-flash"> </embed>
     </div>
 
     <div class="container">
